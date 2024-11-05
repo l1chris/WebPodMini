fe86fcb7ec5f2bba3f0fd82c0ddd0af5f6df89cc
Christian Schwenk <christian-schwenk@gmx.de>
2024-11-05 19:37:44 +0100

    Update song options

--- src/constants/songOptions.ts
+++ src/constants/songOptions.ts
@@ -1,4 +1,7 @@
 export enum SongOption {
-  Song1 = '/music/BetteDavisEyes.mp3',
-  Song2 = '/music/RunningUp.mp3'
+  Song1 = '/music/Altara.mp3',
+  Song2 = '/music/Arsanis.mp3',
+  Song3 = '/music/Felucia.mp3',
+  Song4 = '/music/Nelyth.mp3',
+  Song5 = '/music/Treya.mp3'
 }
