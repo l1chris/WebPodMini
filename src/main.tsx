52691de476ed10da5d5253dbe854ce442508c20e
Christian Schwenk <christian-schwenk@gmx.de>
2024-11-19 19:36:38 +0100

    Delete index.scss, move content to IPod.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
 import IPod from './components/IPod'
 
 createRoot(document.getElementById('root')!).render(
