f9a0465dfca7b92a652aa8108cfa8eda4e102b9d
Christian Schwenk <christian-schwenk@gmx.de>
2024-11-19 19:27:59 +0100

    Remove App component

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import IPod from './components/IPod'
-
-const App: React.FC = () => {
-  return <IPod />
-}
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import App from './App.tsx'
+import IPod from './components/IPod'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <IPod />
   </StrictMode>,
 )
