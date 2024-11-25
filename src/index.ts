433e37e53255ef72ee56c96e854b003d6ab9f1f9
Christian Schwenk <christian-schwenk@gmx.de>
2024-11-25 15:56:27 +0100

    Preparing for distribution

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as IPod } from './components/IPod';
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), dts()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'IPod',
+      fileName: (format) => `ipod.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
